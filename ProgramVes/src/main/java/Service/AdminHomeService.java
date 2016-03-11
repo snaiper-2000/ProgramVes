06f88fb27a869c9fcd1e4cefd2908e04ed4a8206
Dimka <snaiper-2000@mail.ru>
2016-03-11 22:20:56 +0300

    AdminHomeService обн

---
+++ ProgramVes/src/main/java/Service/AdminHomeService.java
@@ -0,0 +1,56 @@
+package Service;
+
+import java.util.List;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import Hibernate.HibernateUtil;
+import Model.Result;
+import Model.User;
+
+public class AdminHomeService {
+	
+	public List<Object []> usersInBd(){
+		Session session = HibernateUtil.openSession();
+		Transaction transaction = null;
+		//String userLoginDB = null;
+		List<Object []> users = null;
+		User user = null;
+		
+		try{
+			transaction = session.getTransaction();
+			transaction.begin();
+			SQLQuery query = session.createSQLQuery("SELECT * FROM user;");
+			//query.setParameter(0, user.getLogin());
+			query.addEntity(Model.User.class);
+			//query.uniqueResult();
+			users = query.list();
+			//System.out.println(resultDate);
+			transaction.commit();
+			
+		}catch(Exception e){
+			//îòìåíÿåì òðàíçàêöèþ
+			transaction.rollback();
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		
+        /*for( users.iterator(); users.iterator().hasNext();){
+    		
+        	user =  (User) users.iterator().next();
+    		//System.out.println(" ResultUserService  âûâîä ôëàãîâ "+result.getFlag1()+" __ "+result.getFlag32());
+    		break;
+    	}
+		
+		/*for(Object [] obj :resultDate ){
+				System.out.println(""+obj[0]);
+			
+    	}*/
+		
+		return users;
+	}
+
+}
